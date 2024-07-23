234d39abf41e861d6f8ae8945ce108f9cbe9c980
Eric Brian Njuguna <eric.njuguna@student.moringaschool.com>
2024-07-23 07:49:36 +0300

    Adds fetch method in index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+
+function submitData(userName, userEmail) {
+    return fetch(`http://localhost:3000/users`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            "name": userName,
+            "email": userEmail
+        })
+    })
+        .then(response => response.json())
+        .then(object => {
+            // const p = document.createElement(`p`);
+            // p.innerText = object.id;
+            document.body.innerHTML = object.id;
+        })
+        .catch(error => {
+            document.body.innerHTML = error.message;
+        });
+}
+
+submitData(`Steve`, `steve@steve.com`)
